b76b927e93a9f8b0b0d0343d9debbedec5e57f0f
Eya-Swayah <eyaswayah000@gmail.com>
2021-02-02 18:46:49 +0100

    second commit

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -11,13 +11,14 @@ export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     
    // { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-   { path: '/patient-list', title: 'Patient List',  icon:'content_paste', class: '' },
+   { path: '/patient-list', title: 'Patients',  icon:'content_paste', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
     { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
     { path: '/typography', title: '',  icon:'', class: '' },
     { path: '/icons', title: '',  icon:'', class: '' },
     { path: '/user-profile', title: ' ',  icon:'', class: '' },
     { path: '/upgrade', title: '',  icon:'', class: 'active-pro' },
+    { path: '/table-list', title: 'Appointments',  icon:'content_paste', class: '' },
 ];
 
 @Component({
